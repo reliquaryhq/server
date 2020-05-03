e646d1ccd2d4ef9fc7a5ab62a9dffea3a59be6a2
fallenoak <git@fallenoak.me>
2020-05-03 12:52:03 -0500

    chore: refactor entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,10 @@
 #!/usr/bin/env node
 
-import Koa from 'koa';
-import Router from '@koa/router';
-import * as db from './util/db';
-
-const app = new Koa();
-const router = new Router();
-
-router.get('/', async (ctx) => {
-  ctx.body = 'Hello World';
-});
+import app from './app';
+import { db } from './util';
 
 const main = async (): Promise<void> => {
   await db.migrate();
-
-  app.use(router.routes());
   app.listen(3000);
 };
 
