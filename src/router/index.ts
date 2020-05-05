b7f74203c3d53888c057cf16247c7a83fb0a3f22
fallenoak <git@fallenoak.me>
2020-05-04 23:43:55 -0500

    feat: add create submission endpoint

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import { router } from '../util';
 import account from './account';
 import auth from './auth';
+import submission from './submission';
 
 const root = router.createRouter();
 
 root.use(account.routes(), account.allowedMethods());
 root.use(auth.routes(), auth.allowedMethods());
+root.use(submission.routes(), submission.allowedMethods());
 
 export default root;
--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,49 @@
 import Koa from 'koa';
 import { SessionContext } from './middleware/session';
 
-type AppState = Koa.DefaultState & AuthState;
 type AppContext = SessionContext<SessionState>;
+
+type AppState = Koa.DefaultState & AuthState;
 
 type AuthState = {
   user?: User;
+};
+
+type CdromDescription = {
+  id?: number;
+  cdromId?: number;
+  sourceId?: number;
+  submissionId?: number;
+  discIndex?: number;
+  labelProductName?: string;
+  labelDiscName?: string;
+  labelLegalese?: string;
+  labelPartNumber?: string;
+  labelVersion?: string;
+  masteringCode?: string;
+  masteringSidCode?: string;
+  toolstampCode?: string;
+  mouldSidCode?: string;
+  notes?: string;
+  createdAt?: number;
+  updatedAt?: number;
+};
+
+type CdromDump = {
+  id?: number;
+  cdromId?: number;
+  copyProtectionId?: number;
+  dumpControllerId?: number;
+  dumpDriveId?: number;
+  dumpFormatId?: number;
+  dumpModificationStateId?: number;
+  dumpReadStateId?: number;
+  dumpToolId?: number;
+  sourceId?: number;
+  submissionId?: number;
+  notes?: string;
+  createdAt?: number;
+  updatedAt?: number;
 };
 
 type SessionState = {
@@ -19,4 +57,11 @@ type User = {
   passwordKeylen: number;
 };
 
-export { AppContext, AppState, SessionState, User };
+export {
+  AppContext,
+  AppState,
+  CdromDescription,
+  CdromDump,
+  SessionState,
+  User,
+};
