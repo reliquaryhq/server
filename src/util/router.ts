165c362615b3fb5e562b4d638b28a76004e55b23
fallenoak <git@fallenoak.me>
2020-05-10 00:22:15 -0500

    chore: switch to definitely typed defs for @koa/router

--- src/util/router.ts
+++ src/util/router.ts
@@ -2,7 +2,7 @@ import KoaRouter from '@koa/router';
 import { AppContext, AppState } from '../types';
 
 const createRouter = (
-  opt?: KoaRouter.IRouterOptions
+  opt?: KoaRouter.RouterOptions
 ): KoaRouter<AppState, AppContext> => new KoaRouter<AppState, AppContext>(opt);
 
 export { createRouter };
