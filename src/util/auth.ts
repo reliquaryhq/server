9a37c603bf7e9a64883a01120f442c4c00ff39ea
fallenoak <git@fallenoak.me>
2020-05-10 16:45:47 -0500

    chore: add PASSWORD_DUMMY_SALT

--- src/util/auth.ts
+++ src/util/auth.ts
@@ -1,8 +1,9 @@
 import { randomBytes } from './crypto';
 
+const PASSWORD_DUMMY_SALT = 'xyzzyXYZZY';
 const PASSWORD_KEYLEN = 64;
 const PASSWORD_SALTLEN = 32;
 
 const generateSalt = (): Promise<Buffer> => randomBytes(PASSWORD_SALTLEN);
 
-export { PASSWORD_KEYLEN, generateSalt };
+export { PASSWORD_DUMMY_SALT, PASSWORD_KEYLEN, generateSalt };
